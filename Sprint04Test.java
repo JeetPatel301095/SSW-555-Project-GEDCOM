f9d21490a0ea42f758272028a06b8b7428e0e351
Chris Rudel <crudel@stevens.edu>
2019-11-04 18:41:02 -0500

    Set up files for Sprint 4

---
+++ Sprint04Test.java
@@ -0,0 +1,21 @@
+import junit.framework.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class Sprint04Test {
+
+
+    @Test
+    public void US34(){
+
+    }
+    @Test
+    public void US35(){
+        
+    }
+}
