5565964fa4c7b256bfa7ec080355438076e9c9b3
Chris Rudel <crudel@stevens.edu>
2019-11-04 20:53:52 -0500

    Finished my user stories. Added a helper method to calculate the days in between two dates. Added sprint04test.ged for the file to parse

--- Sprint04Test.java
+++ Sprint04Test.java
@@ -11,11 +11,37 @@ public class Sprint04Test {
 
 
     @Test
-    public void US34(){
+    public void testUS34(){
+        Sprint4 sprint4 = new Sprint4();
+        ArrayList<Person> individs = new ArrayList<>();
+        ArrayList<Family> fams = new ArrayList<>();
+        Person p1 = new Person();
+        Person p2 = new Person();
+        p1.setId("p01");
+        p2.setId("p02");
+        p1.setBirthDate("1", "MAR", "1920");
+        p2.setBirthDate("1", "JAN", "1950");
+        Family fam1 = new Family();
+        fam1.setId("f01");
+        fam1.setDad(p1);
+        fam1.setMom(p2);
+        fam1.setMarriageDate("1", "SEP", "1980");
+        individs.add(p1);
+        individs.add(p2);
+        fams.add(fam1);
+        sprint4.US34(individs, fams);
 
     }
     @Test
-    public void US35(){
-        
+    public void testUS35(){
+        Sprint4 sprint4 = new Sprint4();
+        ArrayList<Person> individs = new ArrayList<>();
+        Person p1 = new Person();
+        p1.setId("p01");
+        p1.setFirstName("chris");
+        p1.setLastName("rudel");
+        p1.setBirthDate("6", "OCT", "2019");
+        individs.add(p1);
+        sprint4.US35(individs);
     }
 }
--- Sprint4.java
+++ Sprint4.java
@@ -12,6 +12,40 @@ public class Sprint4 {
      *        fams - ArrayList which contains all families
      */
     public void US34(ArrayList<Person> individs, ArrayList<Family> fams){
+        for(Family fam: fams){
+            Date marriageDate = fam.getMarriageDate();
+            if(marriageDate == null){
+                continue;
+            }
+            String dadID = fam.getHusbandId();
+            String momID = fam.getWifeId();
+            Date dadBDay = null;
+            Date momBDay = null;
+            for(Person person: individs){
+                if(person.getId().equals(dadID)){
+                    dadBDay = person.getBirthDate();
+                }
+            }
+            for(Person person: individs){
+                if(person.getId().equals(momID)){
+                    momBDay = person.getBirthDate();
+                }
+            }
+            if(dadBDay == null || momBDay == null){
+                continue;
+            }
+            if(marriageDate.before(dadBDay) || marriageDate.before(momBDay)){
+                System.out.println("ERROR: US34: Father or mother of family ID: " + fam.getId() + " was born after marriage date");
+                continue;
+            }
+            int dadMarriageAge = HelperMethods.calculateYear(dadBDay, marriageDate);
+            int momMarriageAge = HelperMethods.calculateYear(momBDay, marriageDate);
+            if( dadMarriageAge >= momMarriageAge * 2 ){
+                System.out.println("US34: FAMILIES: Family with ID: " + fam.getId() + "s father was more than twice the age of the mother when they got married. (Dad was: " + dadMarriageAge + ", mom was: " + momMarriageAge + ")");
+            }else if( momMarriageAge >= dadMarriageAge * 2 ){
+                System.out.println("US34: FAMILIES: Family with ID: " + fam.getId() + "s mother was more than twice the age of the father when they got married. (Mom was: " + momMarriageAge + ", dad was: " + dadMarriageAge + ")");
+            }
+        }
 
     }
     /*  Chris Rudel US35
@@ -19,6 +53,18 @@ public class Sprint4 {
      * @param individs - ArrayList which contains all individuals
      */
     public void US35(ArrayList<Person> individs){
-
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date today = new Date();
+        //System.out.println(simpleDateFormat.format(today));
+        for(Person person: individs){
+            Date bDate = person.getBirthDate();
+            if(today.before(bDate) || bDate == null || bDate.equals("")){
+                continue;
+            }
+            int days = HelperMethods.calculateDays(today, bDate);
+            if(days <= 30){
+                System.out.println("US35: INDIVIDUALS: Individual with ID: " + person.getId() + " was born within the past 30 days (" + days + " days ago)");
+            }
+        }
     }
 }
